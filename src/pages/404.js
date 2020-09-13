9ae5cd7e87844c0b437c7f339bcaa51752754c1e
ing-arriola <ingeniero.arriola@gmail.com>
2020-09-13 04:54:30 -0600

    Added some test pages :)

---
+++ src/pages/404.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const errorPage = () => {
+  return (
+    <div>
+      <h1>THIS PAGE DOESN'T EXISTS</h1>
+    </div>
+  )
+}
+
+export default errorPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import React from "react"
-
-export default function Home() {
-  return <div>Hello world!</div>
+import { Link } from "gatsby"
+export default () => {
+  return (
+    <div>
+      <h1>HELLO</h1>
+      <Link to="/about/">Nosotros</Link>
+      <Link to="/news/">Noticias</Link>
+    </div>
+  )
 }
