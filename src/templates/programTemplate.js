e91af6760c02f6b28b9eb17970442f68410a5cb7
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-04 14:00:59 -0600

    Added new graphql query to get data from single organization program

--- src/templates/programTemplate.js
+++ src/templates/programTemplate.js
@@ -1,7 +1,29 @@
 import React from "react"
+import { graphql, Link } from "gatsby"
+import { BLOCKS } from "@contentful/rich-text-types"
+import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
+import Layout from "../Components/Layout"
 
 const programTemplate = ({ data }) => {
-  return <div>Hello hello</div>
+  console.log(data)
+  return (
+    <div>
+      <Layout>
+        <div>Holas</div>
+      </Layout>
+    </div>
+  )
 }
+
+export const programQuery = graphql`
+  query getSingleProgram($id: String) {
+    contentfulPrograms(descripcion: { id: { eq: $id } }) {
+      id
+      childContentfulProgramsDescripcionRichTextNode {
+        json
+      }
+    }
+  }
+`
 
 export default programTemplate
