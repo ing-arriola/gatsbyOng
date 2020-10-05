e452038339fe2f24038928ab2c7b9f5b0b71f937
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-05 15:55:30 -0600

    completed get specific program from contentful and rennder on UI

--- src/Components/Program.js
+++ src/Components/Program.js
@@ -1,51 +1,47 @@
 import React from "react"
 import Image from "gatsby-image"
 import { graphql, useStaticQuery, Link } from "gatsby"
-/*
+
 const programsQuery = graphql`
   {
-    allContentfulPrograms {
+    allContentfulOngPrograms {
       nodes {
         id
-        nombre
+        titulo
         imagen {
           fluid {
             ...GatsbyContentfulFluid
           }
         }
-        descripcion {
+        texto {
           id
         }
       }
     }
   }
 `
-*/
+
 const Program = () => {
-  /*  const data = useStaticQuery(programsQuery)
-  console.log(data.allContentfulPrograms.nodes[0].nombre)*/
+  const data = useStaticQuery(programsQuery)
+  console.log(data.allContentfulOngPrograms.nodes[0].titulo)
   return (
     <div>
       <h1>CONOCE NUESTROS PROGRAMAS DE AYUDA</h1>
+      <div className="posts-container">
+        {data.allContentfulOngPrograms.nodes.map(programa => (
+          <section key={programa.id} className="post-card">
+            <div className="post-card__title">
+              <h2>{programa.titulo}</h2>
+            </div>
+            <Image fluid={programa.imagen.fluid} />
+            <div className="post-card__read-more">
+              <Link to={`/programs/${programa.texto.id}`}>Leer mas</Link>
+            </div>
+          </section>
+        ))}
+      </div>
     </div>
   )
 }
 
 export default Program
-
-/*
-
-<div className="posts-container">
-        {data.allContentfulPrograms.nodes.map(programa => (
-          <section key={programa.id} className="post-card">
-            <div className="post-card__title">
-              <h2>{programa.nombre}</h2>
-            </div>
-            <Image fluid={programa.imagen.fluid} />
-            <div className="post-card__read-more">
-              <Link to={`/programs/${programa.descripcion.id}`}>Leer mas</Link>
-            </div>
-          </section>
-        ))}
-      </div>
-*/
--- src/templates/programTemplate.js
+++ src/templates/programTemplate.js
@@ -5,35 +5,57 @@ import { documentToReactComponents } from "@contentful/rich-text-react-renderer"
 import Layout from "../Components/Layout"
 
 const programTemplate = ({ data }) => {
-  console.log(data.contentfulPrograms)
-  /*const programContent = data.contentfulPrograms
+  console.log(
+    data.contentfulOngPrograms.childContentfulOngProgramsTextoRichTextNode
+  )
+  const programContent = data.contentfulOngPrograms
   const options = {
     renderNode: {
       [BLOCKS.HEADING_1]: (node, children) => <h1>{children}</h1>,
       [BLOCKS.HEADING_2]: (node, children) => <h2>{children}</h2>,
       [BLOCKS.HEADING_3]: (node, children) => <h3>{children}</h3>,
       [BLOCKS.HEADING_4]: (node, children) => <h4>{children}</h4>,
+      [BLOCKS.EMBEDDED_ASSET]: (node, children) => (
+        <div className="newsImages-container">
+          <img
+            className="newsImages"
+            src={`https:${node.data.target.fields.file["en-US"].url}`}
+          ></img>
+        </div>
+      ),
     },
     renderMark: {},
-  }*/
+  }
   return (
     <div>
       <Layout>
-        <div className="long-info">Hola</div>
+        <div>
+          <Link to="/programs">Regresar a programas</Link>
+          <div className="article-text">
+            {documentToReactComponents(
+              programContent.childContentfulOngProgramsTextoRichTextNode.json,
+              options
+            )}
+          </div>
+        </div>
       </Layout>
     </div>
   )
 }
-/*
+
 export const programQuery = graphql`
   query getSingleProgram($id: String) {
-    contentfulPrograms(descripcion: { id: { eq: $id } }) {
+    contentfulOngPrograms(texto: { id: { eq: $id } }) {
       id
-      childContentfulProgramsDescripcionRichTextNode {
+      childContentfulOngProgramsTextoRichTextNode {
         json
       }
     }
   }
 `
-*/
 export default programTemplate
+
+/*
+
+
+*/
