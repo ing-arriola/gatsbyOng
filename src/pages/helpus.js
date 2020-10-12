f827f76d4ae6dc4720becbaa33a550b759d2e38f
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-12 17:27:53 -0600

    Added test data and now icons and test data is redered dinamically

--- src/pages/helpus.js
+++ src/pages/helpus.js
@@ -1,25 +1,48 @@
 import React from "react"
 import Layout from "../Components/Layout"
-import { FaHandHoldingHeart, FaHandPaper } from "react-icons/fa"
+import * as FontAwesome from "react-icons/fa"
 const helpus = () => {
+  const helpWays = [
+    {
+      name: "Patrocinador",
+      description:
+        "Puedes realizar un aporte economico a nuestra cuenta del banco agricola",
+      icon: "FaHandHoldingHeart",
+      size: 70,
+    },
+    {
+      name: "Voluntario",
+      description:
+        "Puedes ayudar con tu tiempo colaborando en nuestros programas de apoyo a la comunidad",
+      icon: "FaHandPaper",
+      size: 70,
+    },
+  ]
+
   return (
     <Layout>
       <div className="helpContainer">
         <h1>Ayudanos a ayudar</h1>
         <p>
           Constantemente nos encontramos pensando en planes de accion para
-          beneficiar a quinenes lo necesitan, tu tambien puedes ayudar como:
+          beneficiar a quinenes lo necesitan.
         </p>
-        <ul>
-          <li>
-            {" "}
-            <FaHandHoldingHeart size={70} /> Patrocinador
-          </li>
-          <li>
-            {" "}
-            <FaHandPaper size={70} /> Voluntario{" "}
-          </li>
-        </ul>
+        <div>
+          {helpWays.map(helpWay => (
+            <section className="help-section">
+              <div className="help-section--title">
+                <div style={{ fontSize: helpWay.size }}>
+                  {React.createElement(FontAwesome[helpWay.icon])}
+                </div>
+
+                {helpWay.name + helpWay.size}
+              </div>
+              <div className="help-section--description">
+                {helpWay.description}
+              </div>
+            </section>
+          ))}
+        </div>
       </div>
     </Layout>
   )
