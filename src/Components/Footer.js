f3c6b3bd5d0a931c7563cba9f2188490d0447f62
ing-arriola <ingeniero.arriola@gmail.com>
2020-10-15 15:55:26 -0600

    Added initial info into footer and some styles too

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,10 +1,19 @@
 import React from "react"
-
+import { FaFacebook, FaRegEnvelope, FaWhatsapp } from "react-icons/fa"
 const Footer = () => {
   return (
-    <div>
-      <h2>Hello from footer</h2>
-    </div>
+    <footer className="main-footer">
+      <div>
+        <p>Website builded by: Jaime Arriola</p>
+        <p>Red de jóvenes del futuro &copy; </p>
+      </div>
+
+      <div>
+        <FaFacebook size={40} />
+        <FaRegEnvelope size={40} />
+        <FaWhatsapp size={40} />
+      </div>
+    </footer>
   )
 }
 
