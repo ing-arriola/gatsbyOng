e1058c8ddc4d1c16743736d30397d62ae7b67f7a
ing-arriola <ingeniero.arriola@gmail.com>
2020-11-20 12:28:31 -0600

    improved styles and deleted unnecesary pictures

--- src/Components/OngInfo.js
+++ src/Components/OngInfo.js
@@ -1,6 +1,12 @@
 import React from "react"
+import Img from "gatsby-image"
 import { graphql, useStaticQuery } from "gatsby"
 import * as FontAwesome from "react-icons/fa"
+
+import mision from "../images/About/mision.svg"
+import vision from "../images/About/vision.svg"
+import valores from "../images/About/valores.svg"
+import politicas from "../images/About/politicas.svg"
 
 const infoQuery = graphql`
   {
@@ -36,7 +42,7 @@ const OngInfo = () => {
   /* Turning a paragraph into an array in order to add styles to each NGO's "Valor" */
   let prevText = sortedArray.filter(chunk => chunk.name === "Valores")
   let enteredText = prevText[0].description.description
-  const valores = enteredText.split("\n")
+  const valoresOng = enteredText.split("\n")
   /* Array for valores icons */
   const icons = [
     "FaHandsHelping",
@@ -45,44 +51,38 @@ const OngInfo = () => {
     "FaRegThumbsUp",
   ]
 
+  const images = [mision, vision, valores, politicas]
+
   return (
-    <div>
-      {sortedArray.map(info => (
-        <article
-          key={info.id}
-          className={
-            info.name === "Politicas" ? "info-politicas" : "info-container"
-          }
-          id={info.name}
-        >
-          <div className="info-container--title">
-            <p>{info.name}</p>
+    <div className="generic-section">
+      {sortedArray.map((info, index) => (
+        <article key={info.id} className="generic-inner row">
+          <div className="column image-align ">
+            <img className="image-sections" src={images[index]} />
           </div>
 
-          <div className="info-container--ong-data">
-            <div className="ong-data--image"></div>
+          <div className="column">
+            <div className="info-container--title">
+              <p>{info.name}</p>
+            </div>
 
-            <div
-              data-sal="fade"
-              data-sal-duration="800"
-              data-sal-delay="300"
-              data-sal-easing="ease"
-              className={"ong-data-" + info.name}
-            >
-              {info.name === "Valores" ? (
-                valores.map((valor, index) => (
-                  <div className="ong-data-Valores--content">
-                    <div className="valores-icons">
-                      {React.createElement(FontAwesome[icons[index]])}
+            <div>
+              <div className={"ong-data-" + info.name}>
+                {info.name === "Valores" ? (
+                  valoresOng.map((valor, index) => (
+                    <div className="ong-data-Valores--content">
+                      <div className="valores-icons">
+                        {React.createElement(FontAwesome[icons[index]])}
+                      </div>
+                      <span key={index}>{valor}</span>
                     </div>
-                    <span key={index}>{valor}</span>
-                  </div>
-                ))
-              ) : (
-                <p className="info-container--description">
-                  {info.description.description}
-                </p>
-              )}
+                  ))
+                ) : (
+                  <p className="info-container--description">
+                    {info.description.description}
+                  </p>
+                )}
+              </div>
             </div>
           </div>
         </article>
